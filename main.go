b25899aa585edee304ee5e5ed51f18869a5c728a
PikaThorachu <krullwarriorking@gmail.com>
2025-01-31 09:25:39 -0500

    Created func commandExit() and commandHelp(), mapped. Created for loop to check when command the user inputs, included error handling.

--- main.go
+++ main.go
@@ -15,14 +15,65 @@ func cleanInput(text string) []string {
 	return splitwords
 }
 
+func commandExit() error {
+	fmt.Printf("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	for command := range commands {
+		fmt.Printf("%s: %s\n", commands[command].name, commands[command].description)
+	}
+	return nil
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands map[string]cliCommand
+
+func init() {
+	commands = map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
 func main() {
+
 	bufio.NewScanner(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
-		scanner := bufio.NewScanner(os.Stdin)
 		scanner.Scan()
 		text := scanner.Text()
 		words := cleanInput(text)
-		fmt.Printf("Your command was: %s\n", words[0])
+		if len(words) == 0 {
+			continue
+		}
+
+		command := words[0]
+		if cmd, ok := commands[command]; ok {
+			err := cmd.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			fmt.Printf("Unknown command\n")
+		}
 	}
 }
